c1883ac79f7a8ef50684aa60c622d55a2c39769e
Peter Gemmell <petergemmell65@hotmail.com>
2020-08-17 15:25:17 +0100

    dependencies added

---
+++ src/test/java/com/example/schoolsystem/schoolservice/SchoolserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolsystem.schoolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
