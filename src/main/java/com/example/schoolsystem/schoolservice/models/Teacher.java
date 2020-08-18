45093ac9f7e27c3168ef87479f9fd056ada6568a
Peter Gemmell <petergemmell65@hotmail.com>
2020-08-18 15:02:33 +0100

    Working on teacher class

--- src/main/java/com/example/schoolsystem/schoolservice/models/Teacher.java
+++ src/main/java/com/example/schoolsystem/schoolservice/models/Teacher.java
@@ -1,4 +1,28 @@
 package com.example.schoolsystem.schoolservice.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="teachers")
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="teacher_name")
+    private String teacherName;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    @Column(name="teacher_address")
+    private String teacherAddress;
+
+    @JsonIgnoreProperties(value = "teachers")
+    @OneToMany(mappedBy = "teacher", fetch = FetchType.LAZY)
+    private List<Exam> exams;
 }
