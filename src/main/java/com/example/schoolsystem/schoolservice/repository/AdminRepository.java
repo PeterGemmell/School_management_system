0b9a453e099603d19261a80e42c6f8e992474acf
Peter Gemmell <petergemmell65@hotmail.com>
2020-08-20 10:33:38 +0100

    admin repository complete

--- src/main/java/com/example/schoolsystem/schoolservice/repository/AdminRepository.java
+++ src/main/java/com/example/schoolsystem/schoolservice/repository/AdminRepository.java
@@ -1,4 +1,9 @@
 package com.example.schoolsystem.schoolservice.repository;
 
-public interface AdminRepository {
+import com.example.schoolsystem.schoolservice.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Admin findByFullNameAndPassword(String fullName, String password);
 }
